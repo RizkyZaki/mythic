a627e59b93ea69ec2c4ffb90f875bc6acf6cff50
RizkyZaki <ifsry416@gmail.com>
2023-10-16 15:58:02 +0700

    :sparkles: Adding Modul 1

---
+++ S1/Praktikum PPL/Modul 1/tugas_1.py
@@ -0,0 +1,8 @@
+n = int(input("Masukkan Jumlah Ikan Cupang : "))
+m = int(input("Masukkan Jumlah Cucu Pak Dengklek : "))
+
+x = n/m
+y = n%m
+
+print(f"Ikan Cupang Dibagi Masing Masing {int(x)}")
+print(f"Bersisa {int(y)}")
