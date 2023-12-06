8eac8b2736f3d85e5394ee37f7b248b532313ca3
RizkyZaki <ifsry416@gmail.com>
2023-12-06 16:13:17 +0700

    :sparkles:

--- S1/UAS/parkir.py
+++ S1/UAS/parkir.py
@@ -3,18 +3,42 @@ import datetime
 # Konstanta
 TARIF_PER_DETIK = 10000
 MAKSIMAL_WAKTU_PARKIR = 240
-PIN_ADMIN_PARKIR = "1234"
+MAX_TRIES = 4
+PIN_ADMIN_PARKIR = "123456"
 
-# Inisialisasi data parkir
 parking_data = {}
+parking_history = []
 
-# Fungsi untuk mencatat kendaraan masuk
+def hitung_biaya_parkir(durasi_parkir):
+    # Pembulatan waktu parkir
+    durasi_parkir = max(round(durasi_parkir / 60) * 60, 60)
+
+    # Perhitungan biaya parkir
+    biaya_parkir = TARIF_PER_DETIK * (durasi_parkir / 60)
+
+    # Perhitungan denda
+    if durasi_parkir > MAKSIMAL_WAKTU_PARKIR:
+        denda = biaya_parkir * 0.1
+        durasi_parkir = max(round(durasi_parkir / 60) * 60, 360)
+    elif durasi_parkir > 360:
+        denda = biaya_parkir * 0.25
+        durasi_parkir = max(round(durasi_parkir / 60) * 60, 480)
+    else:
+        denda = 0
+
+    # Perhitungan biaya parkir setelah penanganan denda dan pembulatan
+    biaya_parkir = TARIF_PER_DETIK * (durasi_parkir / 60)
+
+    total_biaya = biaya_parkir + denda
+
+    return total_biaya, durasi_parkir, denda
+
+
 def kendaraan_masuk(nomor_plat):
     waktu_masuk = datetime.datetime.now()
     parking_data[nomor_plat] = {"waktu_masuk": waktu_masuk}
     print(f"Kendaraan dengan nomor plat {nomor_plat} masuk. Silahkan masuk.")
 
-# Fungsi untuk mencatat kendaraan keluar dan menghitung biaya parkir
 def kendaraan_keluar(nomor_plat):
     if nomor_plat not in parking_data:
         print("Error: Kendaraan tidak tercatat masuk.")
@@ -23,30 +47,15 @@ def kendaraan_keluar(nomor_plat):
     waktu_keluar = datetime.datetime.now()
     waktu_masuk = parking_data[nomor_plat]["waktu_masuk"]
     durasi_parkir = (waktu_keluar - waktu_masuk).total_seconds()
-    
-    # Pembulatan waktu parkir
-    durasi_parkir = min(round(durasi_parkir / 60) * 60, MAKSIMAL_WAKTU_PARKIR)
 
-    # Perhitungan biaya parkir
-    biaya_parkir = TARIF_PER_DETIK * (durasi_parkir / 60)
+    total_biaya, durasi_parkir, denda = hitung_biaya_parkir(durasi_parkir)
 
-    # Perhitungan denda
-    if durasi_parkir > MAKSIMAL_WAKTU_PARKIR:
-        denda = biaya_parkir * 0.1
-    elif durasi_parkir > 360:
-        denda = biaya_parkir * 0.25
-    else:
-        denda = 0
-
-    total_biaya = biaya_parkir + denda
-
-    # Tampilkan biaya parkir
     print(f"Biaya Parkir: Rp {total_biaya}")
+    if denda > 0:
+        print(f"Denda Parkir: Rp {denda}")
 
-    # Input nominal pembayaran
     nominal_pembayaran = float(input("Masukkan nominal pembayaran: Rp "))
-    
-    # Penanganan alternatif flow untuk penanganan error
+
     while nominal_pembayaran < total_biaya:
         print("Error: Nominal pembayaran kurang. Silahkan masukkan nominal pembayaran yang cukup.")
         nominal_pembayaran = float(input("Masukkan nominal pembayaran: Rp "))
@@ -55,30 +64,51 @@ def kendaraan_keluar(nomor_plat):
 
     print(f"Terima Kasih! Kembalian Anda: Rp {kembalian}")
 
-    # Hapus data parkir setelah kendaraan keluar
+    parking_history.append({
+        "nomor_plat": nomor_plat,
+        "waktu_masuk": waktu_masuk,
+        "waktu_keluar": waktu_keluar,
+        "durasi_parkir": durasi_parkir,
+        "biaya_parkir": total_biaya,
+        "denda": denda
+    })
+
     del parking_data[nomor_plat]
 
-# Fungsi untuk menu Admin Parkir
-def admin_parkir(pin):
-    if pin == PIN_ADMIN_PARKIR:
+def admin_parkir():
+    tries = 0
+    while tries < MAX_TRIES:
+        entered_pin = input("Masukkan PIN Admin Parkir: ")
+
+        if entered_pin == PIN_ADMIN_PARKIR:
+            print("PIN Admin Parkir Benar.")
+            break
+        else:
+            print("Error: PIN salah. Kesempatan mencoba tersisa: ", MAX_TRIES - tries - 1)
+            tries += 1
+
+    if tries == MAX_TRIES:
+        print("Anda melebihi jumlah maksimal percobaan. Kembali ke Menu Utama.")
+        return
+
+    while True:
         print("=== ADMIN PARKIR ===")
         print("1. Cetak Seluruh Transaksi Parkir")
         print("2. Kembali ke Menu Utama")
         pilihan = input("Pilih menu: ")
 
         if pilihan == "1":
-            for nomor_plat, data in parking_data.items():
-                print(f"Nomor Plat: {nomor_plat}, Waktu Masuk: {data['waktu_masuk']}")
-
+            if not parking_history:
+                print("Belum ada kendaraan yang parkir.")
+            else:
+                for data in parking_history:
+                    print(f"Nomor Plat: {data['nomor_plat']}, Waktu Masuk: {data['waktu_masuk']}, Waktu Keluar: {data['waktu_keluar']}, Durasi Parkir: {data['durasi_parkir']} detik, Biaya Parkir: Rp {data['biaya_parkir']}, Denda: Rp {data['denda']}")
         elif pilihan == "2":
-            return
+            break
+       
         else:
             print("Pilihan tidak valid.")
-            admin_parkir(pin)
-    else:
-        raise ValueError("Error: PIN salah.")
 
-# Perulangan untuk menu utama
 while True:
     print("=== MENU UTAMA ===")
     print("1. Masuk Area Parkir")
@@ -87,7 +117,6 @@ while True:
     print("4. Keluar")
     pilihan = input("Pilih menu: ")
 
-    # Percabangan
     if pilihan == "1":
         nomor_plat = input("Masukkan nomor plat kendaraan: ")
         kendaraan_masuk(nomor_plat)
@@ -97,13 +126,7 @@ while True:
         kendaraan_keluar(nomor_plat)
 
     elif pilihan == "3":
-        pin = input("Masukkan PIN Admin Parkir: ")
-
-        # Exception handling
-        try:
-            admin_parkir(pin)
-        except ValueError as e:
-            print("Error: PIN Tidak Valid")
+        admin_parkir()
 
     elif pilihan == "4":
         break
